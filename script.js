001bc11a4ffa0f2bba33bd902dee02c1bacb7ee2
ValentinaVelkova <velkova.v01@gmail.com>
2024-04-12 15:00:43 +0300

    Fix bugs

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const randomNumberGenerator = (number) => {
 const fortunes = {
     personalityTrait: 
     [
-        'Humble', 'Honorable', 'Loayal', 'Patient', 'Curios', 'Persistent', 'Disciplined'
+        'Humble', 'Honorable', 'Loyal', 'Patient', 'Curios', 'Persistent', 'Disciplined'
     ],
     needMoreWork: 
     [
@@ -22,10 +22,10 @@ const fortunes = {
     ],
     quote: 
     [
-        'It takes courage to grow up and become who you really are.','Your self-worth is determined by you.', 
-        'Keep your face always toward the sunshine, and shadows will fall behind you.', 
-        'Champions keep playing until they get it right.', 'Believe you can and you are halfway there.',
-        'It is during our darkest moments that we must focus to see the light.'
+        ' "It takes courage to grow up and become who you really are!"','"Your self-worth is determined by you!"', 
+        '"Keep your face always toward the sunshine, and shadows will fall behind you!"', 
+        '"Champions keep playing until they get it right!"', '"Believe you can and you are halfway there!"',
+        '"It is during our darkest moments that we must focus to see the light!"'
     ]
 }
 
@@ -35,17 +35,17 @@ let messages = [];
 for (let message in fortunes) {
     let value = fortunes[message];
     let randomIndex = randomNumberGenerator(value.length);
-    messages = push(value[randomIndex]);
+    messages.push(value[randomIndex]);
 }
 
 // Main function
 
 const fortunesMessage = (arr) => {
     let finalMessage = [];
-    finalMessage.push(`As predicted your best personality trait is ${arr[0]}`);
-    finalMessage.push(`That is great but you shoud work more on your ${arr[1]}`);
-    finalMessage.push(`Don't worry though because you will succeed in ${arr[2]}.`);
-    finalMessage.push(`Your quote of the day is ${arr[3]}`);
+    finalMessage.push(`\u{1F60D} As predicted your best personality trait is: ${arr[0]}`);
+    finalMessage.push(`\u{1F613} That is great but you shoud work more on your: ${arr[1]}`);
+    finalMessage.push(`\u{1F60E} Don't worry though, because you will succeed in: ${arr[2]}`);
+    finalMessage.push(`\u{1F607} Your quote of the day is: ${arr[3]}`);
 
     const messageString = finalMessage.join('\n');
     console.log(messageString);
@@ -53,11 +53,4 @@ const fortunesMessage = (arr) => {
 
 fortunesMessage(messages);
 
-
-
-
-
-/*As predicted your best personality trait is {}. That is great but you
-shoud work more on your {}. Don't worry though because you will succeed in {}.
-Your quote of the day is: {} */
 
